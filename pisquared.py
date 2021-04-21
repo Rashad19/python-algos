7edb53b184a33d9e5b9226a0e4d9ccd2ddbb49c9
Rashad Salim <sc19rsaa@leeds.ac.uk>
2021-04-21 13:14:12 +0400

    initial helper functions

---
+++ pisquared.py
@@ -0,0 +1,28 @@
+
+import math
+
+#This function calculates pisquared using the partial sum
+#(1/6)*piˆ2 = sum from 1 to k of 1/kˆ2
+def pisquared(n):
+
+	result = 0.0
+	i = 1.0;
+	while(i<=n):
+		result += 1/(i**2)
+		i += 1
+
+	pisquare = result*6.0
+
+	return pisquare
+
+
+def pi(n):
+
+	pi = math.sqrt(pisquared(n))
+	print('pi is %f' %pi)
+	return pi
+
+
+pi(600)	
+
+
